084023bda1e64a63a3194154336006c34c8e5f71
Anton Rashchepkin <anton.r@2muchcoffee.com>
2018-08-21 17:03:59 +0300

    feat(lib): create interface for default function names

---
+++ projects/ngx-destroy-subscribers/src/lib/interface.ts
@@ -0,0 +1,10 @@
+/**
+ * Interfaces helping with default function naming
+ */
+export interface AddSubscribers {
+  addSubscribers(): void;
+}
+
+export interface RemoveSubscribers {
+  removeSubscribers(): void;
+}
--- projects/ngx-destroy-subscribers/src/public_api.ts
+++ projects/ngx-destroy-subscribers/src/public_api.ts
@@ -3,3 +3,4 @@
  */
 
 export * from './lib/decorator';
+export * from './lib/interface';
