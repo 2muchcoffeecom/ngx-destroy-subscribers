a404f97b12e3db905a431c3eac099fde6570fe53
Stanislav Mikhniev <stas@2muchcoffee.com>
2019-03-06 14:01:18 +0200

    feat(projects:ngx-destroy-subscribers:src:lib): add ability to pasass optional parameters to Decorator

--- projects/ngx-destroy-subscribers/src/lib/decorator.ts
+++ projects/ngx-destroy-subscribers/src/lib/decorator.ts
@@ -1,42 +1,46 @@
 import { Unsubscribable } from 'rxjs';
 import 'reflect-metadata';
 
-export function DestroySubscribers<TFunction extends Function>(target: TFunction) {
+export function DestroySubscribers(params?) {
   
-  const unsubscribableLike: {subscriptions: Unsubscribable[], unsubscribe: () => void} = {
-    subscriptions: [],
-    unsubscribe,
-  };
-  const subscriber: string = Reflect.getMetadata('subscription:name', target.prototype, 'subscriber');
-  
-  Object.defineProperty(target.prototype, subscriber ? subscriber : 'subscriber', {
-    get: () => unsubscribableLike,
-    set: subscription => unsubscribableLike.subscriptions.push(subscription),
-  });
-  
-  if (typeof target.prototype.ngOnDestroy !== 'function') {
-    throw new Error(`${target.prototype.constructor.name} must implement ngOnDestroy() lifecycle hook`);
-  }
-  
-  target.prototype.ngOnDestroy = ngOnDestroyDecorator(target.prototype.ngOnDestroy);
-  
-  function ngOnDestroyDecorator(f) {
-    return function () {
-      unsubscribe();
-      return f.apply(this, arguments);
+  return function (target) {
+    const unsubscribableLike: {subscriptions: Unsubscribable[], unsubscribe: () => void} = {
+      subscriptions: [],
+      unsubscribe,
     };
-  }
-
-  function unsubscribe() {
-    do {
-      const sub: Unsubscribable = unsubscribableLike.subscriptions.shift();
-      sub.unsubscribe();
-    } while (unsubscribableLike.subscriptions.length);
-  }
-
-  return target;
+    const subscriber: string = Reflect.getMetadata('subscription:name', target.prototype, 'subscriber');
+  
+    Object.defineProperty(target.prototype, subscriber ? subscriber : 'subscriber', {
+      get: () => unsubscribableLike,
+      set: subscription => unsubscribableLike.subscriptions.push(subscription),
+    });
+  
+    if (typeof target.prototype.ngOnDestroy !== 'function') {
+      throw new Error(`${target.prototype.constructor.name} must implement ngOnDestroy() lifecycle hook`);
+    }
+  
+    target.prototype.ngOnDestroy = ngOnDestroyDecorator(target.prototype.ngOnDestroy);
+  
+    function ngOnDestroyDecorator(f) {
+      return function () {
+        unsubscribe();
+        return f.apply(this, arguments);
+      };
+    }
+  
+    function unsubscribe() {
+      do {
+        const sub: Unsubscribable = unsubscribableLike.subscriptions.shift();
+        sub.unsubscribe();
+      } while (unsubscribableLike.subscriptions.length);
+    }
+  
+    return target;
+  };
 }
 
-export function CombineSubscriptions(target: Object, propertyKey: string | symbol) {
-  Reflect.defineMetadata('subscription:name', propertyKey, target, 'subscriber');
+export function CombineSubscriptions(params?) {
+  return function (target, propertyKey: string | symbol) {
+    Reflect.defineMetadata('subscription:name', propertyKey, target, 'subscriber');
+  };
 }
