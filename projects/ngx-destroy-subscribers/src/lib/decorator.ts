799ad199594584f64d7e775abf7c07b5621c22b7
Logvin Oleg <logvinoleg89@gmail.com>
2019-04-25 17:21:23 +0300

    fix(decorator): add context to subscription array

--- projects/ngx-destroy-subscribers/src/lib/decorator.ts
+++ projects/ngx-destroy-subscribers/src/lib/decorator.ts
@@ -1,44 +1,54 @@
 import { Unsubscribable } from 'rxjs';
 import 'reflect-metadata';
 
+export interface InstanceSubscriptionWithInstance {
+  instance: any;
+  subscription: Unsubscribable;
+}
+
 export function DestroySubscribers(params?) {
-  
+
   return function (target) {
     params = {
       destroyFunc: 'ngOnDestroy',
       ...params
     };
-    const unsubscribableLike: {subscriptions: Unsubscribable[], unsubscribe: () => void} = {
+    const unsubscribableLike: {subscriptions: InstanceSubscriptionWithInstance[], unsubscribe: () => void} = {
       subscriptions: [],
       unsubscribe,
     };
     const subscriber: string = Reflect.getMetadata('subscription:name', target.prototype, 'subscriber');
-  
+
     Object.defineProperty(target.prototype, subscriber || 'subscriber', {
       get: () => unsubscribableLike,
-      set: subscription => unsubscribableLike.subscriptions.push(subscription),
+      set: subscription => unsubscribableLike.subscriptions.push({ subscription, instance: this}),
     });
-  
+
     if (typeof target.prototype[params.destroyFunc] !== 'function') {
       throw new Error(`${target.prototype.constructor.name} must implement ${params.destroyFunc}() lifecycle hook`);
     }
-  
+
     target.prototype[params.destroyFunc] = ngOnDestroyDecorator(target.prototype[params.destroyFunc]);
-  
+
     function ngOnDestroyDecorator(f) {
       return function () {
-        unsubscribe();
-        return f.apply(this, arguments);
+        unsubscribe.call(this);
+        return f.apply(target, arguments);
       };
     }
-  
+
     function unsubscribe() {
+      const subscriptionsWithInstances = unsubscribableLike.subscriptions;
+      const escapedSubscriptions = subscriptionsWithInstances.filter(
+        (sub) => sub && typeof sub.subscription.unsubscribe === 'function'
+      );
+      const currentSubscriptions = escapedSubscriptions.filter((sub) => sub.instance === this);
       do {
-        const sub: Unsubscribable = unsubscribableLike.subscriptions.shift();
-        if ( sub && typeof sub.unsubscribe === 'function') { sub.unsubscribe(); }
-      } while (unsubscribableLike.subscriptions.length);
+        const {subscription} = currentSubscriptions.shift();
+        subscription.unsubscribe()
+      } while (currentSubscriptions.length);
     }
-  
+
     return target;
   };
 }
